2267cb33be546079ec72a45762a8a9ab6200bfaf
kasperfred <kasper@kasperfred.com>
2018-05-07 14:38:57 +0200

    refactoring

--- socrates/autodiff_graph.py
+++ socrates/autodiff_graph.py
@@ -6,43 +6,49 @@ Supports two modes
         We use reverse mode
         Yet the graph method is still inefficient
 """
-import math
+import numpy as np
 
-class Variable:
+class Expr():
     def __init__(self, value):
         self.value = value
-        self.children = []
+        self.dependencies = []
         self.grad_value = None
-
-    def grad(self):
-        if self.grad_value is None:
-            self.grad_value = sum(weight * var.grad()
-                                  for weight, var in self.children)
-        return self.grad_value
-
+    
     def __add__(self, other):
         z = Variable(self.value + other.value)
-        self.children.append((1.0, z))
-        other.children.append((1.0, z))
+        self.dependencies.append((1.0, z))
+        other.dependencies.append((1.0, z))
         return z
 
     def __mul__(self, other):
         z = Variable(self.value * other.value)
-        self.children.append((other.value, z))
-        other.children.append((self.value, z))
+        self.dependencies.append((other.value, z))
+        other.dependencies.append((self.value, z))
         return z
 
+class Variable(Expr):
+    def __init__(self, value):
+        Expr.__init__(self, value)
+
+    def grad(self):
+        if self.grad_value is None:
+            self.grad_value = sum(weight * var.grad()
+                                  for weight, var in self.dependencies)
+        return self.grad_value
+
+class Constant(Expr):
+    def __init__(self, value):
+        Expr.__init__(self, value)
+        self.grad_value = 1
+
+    def grad(self):
+        return self.grad_value
+
+
 def sin(x):
-    z = Variable(math.sin(x.value))
-    x.children.append((math.cos(x.value), z))
+    z = Variable(np.sin(x.value))
+    x.dependencies.append((np.cos(x.value), z))
     return z
-
-x = Variable(10)
-y = Variable(9)
-z = x * y + sin(x) * x
-
-z.grad_value = 1
-print (x.grad())
 
 def grad(x):
     """Computes the gradient with respect to variable
@@ -51,15 +57,23 @@ def grad(x):
         x (Variable): Variable with respect to which to compute the gradient
     """
 
-    def deepest_child_grad_value(x):
-        for child in x.children:
-            if not child.children:
-                child.grad_value = 1
+    def deepest_dependency_grad_value(x):
+        for dependency in x.dependencies:
+            var = dependency[1]
+            if not var.dependencies:
+                var.grad_value = 1
                 return
             else:
-                deepest_child_grad_value(child)
+                deepest_dependency_grad_value(var)
     
+    deepest_dependency_grad_value(x)
+
     return x.grad()
+
+x = Variable(np.array([5,4,2]))
+#y = Variable(np.array([1,2,3]))
+#z = x * y + x
+f = sin(x)
 
 print (grad(x))
 
